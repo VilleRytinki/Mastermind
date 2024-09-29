511183fb9706ba4c562b69d91b3de292a8913f1c
Ville Rytinki <66476143+VilleRytinki@users.noreply.github.com>
2024-09-29 10:47:35 +0300

    moved the main game class to its own file

---
+++ lib/mastermind.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Main game loop control for mastermind
+class MasterMind < Gosu::Window
+  def initialize
+    super 2560, 1440, fullscreen: true
+    self.caption = 'MasterMind The Game'
+  end
+
+  def update
+    # main game logic here according to documentation
+  end
+
+  def draw
+    # ...
+  end
+end
