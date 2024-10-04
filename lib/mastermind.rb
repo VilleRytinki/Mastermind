bc3633dcd9a88857b814ca9ae9032363732d4a50
Ville Rytinki <66476143+VilleRytinki@users.noreply.github.com>
2024-10-04 19:17:13 +0300

    Background and closing
    Brought a suitable background to project and created functionality for exiting the game.

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -3,8 +3,10 @@
 # Main game loop control for mastermind
 class MasterMind < Gosu::Window
   def initialize
-    super 2560, 1440, fullscreen: true
+    super 2304, 1296, fullscreen: true
     self.caption = 'MasterMind The Game'
+
+    @background_image = Gosu::Image.new('assets/images/origbig.png')
   end
 
   def update
@@ -12,6 +14,10 @@ class MasterMind < Gosu::Window
   end
 
   def draw
-    # ...
+    @background_image.draw(0, 0, 0)
+  end
+
+  def button_down(button)
+    close if button == Gosu::KB_ESCAPE
   end
 end
