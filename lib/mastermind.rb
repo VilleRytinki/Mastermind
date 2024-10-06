66db29a3b32ebe8222e46ca545573d21413cd48e
Ville Rytinki <66476143+VilleRytinki@users.noreply.github.com>
2024-10-06 13:24:12 +0300

    some trial code to see how the model works

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative 'player_bulb'
 # Main game loop control for mastermind
 class MasterMind < Gosu::Window
   def initialize
@@ -7,14 +8,20 @@ class MasterMind < Gosu::Window
     self.caption = 'MasterMind The Game'
 
     @background_image = Gosu::Image.new('assets/images/origbig.png')
+    @player_bulb = PlayerBulb.new
   end
 
   def update
     # main game logic here according to documentation
+    @player_bulb.color = Gosu::Color::GRAY if Gosu.button_down? Gosu::KB_SPACE
   end
 
   def draw
     @background_image.draw(0, 0, 0)
+    bulb_x = 300
+    bulb_y = 300
+    Gosu.draw_rect(bulb_x, bulb_y, @player_bulb.width, @player_bulb.height,
+                   @player_bulb.color)
   end
 
   def button_down(button)
