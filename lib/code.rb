56ca5e01631aba7c980348ccc866a8d6f83b8b4e
Ville Rytinki <66476143+VilleRytinki@users.noreply.github.com>
2024-11-23 20:53:54 +0200

    updated code class
    code now determines the coordinates of the bulbs in relation to its own location on the board

--- lib/code.rb
+++ lib/code.rb
@@ -1,17 +1,28 @@
 # frozen_string_literal: true
 
-require 'player_bulb'
+require_relative 'player_bulb'
 # This class should consist of bulbs which forms a code.
 class Code
   attr_accessor :bulbs
 
-  def initialize(bulbs)
+  def initialize(bulbs, x_location, y_location)
     @bulbs = bulbs
+    @x_location = x_location
+    @y_location = y_location
   end
 
-  def self.create_blank_code(colors)
+  def self.create_blank_code(colors, x_location, y_location)
     bulbs = []
     4.times { bulbs << PlayerBulb.new(colors.first, colors, 0, 0) }
-    new(bulbs)
+    new(bulbs, x_location, y_location)
+  end
+
+  def set_bulb_locations
+    increment_x = 0
+    @bulbs.each do |bulb|
+      bulb.x = @x_location + increment_x
+      bulb.y = @y_location
+      increment_x += 40
+    end
   end
 end
